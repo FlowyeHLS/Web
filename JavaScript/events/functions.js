ded5d045e8cad042f23a1dd536fbfdb010316fbf
User <User@DESKTOP-6KNLMQJ>
2026-01-21 15:40:04 +0300

    Duration and TimeStamp DONE

--- JavaScript/events/functions.js
+++ JavaScript/events/functions.js
@@ -36,4 +36,80 @@ function switchBackground()
 
     document.getElementById("switch-background").src = `img/${file === "moon.png" ? "sun.png" : "moon.png"}`;
     /*document.getElementById("switch-background").src = file === "moon.png" ? "img/sun.png" : "img/moon.png"*/
+
+
+}
+document.getElementById("btn-start").onclick = function startCountdownTimer()
+{
+    let targetDate = document.getElementById("target-date");
+    let targetTime = document.getElementById("target-time");
+    let btnStart = document.getElementById("btn-start");
+    if (btnStart.value === "Start")
+    {
+        btnStart.value = "Stop";
+        targetDate.disabled = targetTime.disabled = true;
+        tickCountdown();
+    }
+    else
+    {
+        btnStart.value = "Start";
+        targetDate.disabled = targetTime.disabled = false;
+    }
+    //function tickCountdown()
+    //{
+    //    let now = new Date();
+
+    //    let targetDateControl = document.getElementById("target-date");
+    //    let targetTimeControl = document.getElementById("target-time");
+
+    //    let targetDateValue = targetDateControl.valueAsDate;
+    //    let targetTimeValue = targetTimeControl.valueAsDate;
+
+    //    document.getElementById("target-date-value").innerHTML = targetDateValue;
+    //    document.getElementById("target-time-value").innerHTML = targetTimeValue;
+
+
+    //    document.getElementById("duration").innerHTML = typeof (targetTimeValue);
+    //    targetTimeValue.setFullYear(targetDateValue.getFullYear());
+    //    targetTimeValue.setMonth(targetDateValue.getMonth());
+    //    targetTimeValue.setDate(targetDateValue.getDate());
+
+    //    /*  console.log(`${targetDateValue}\t${targetTimeValue}`);*/
+
+    //    let duration = targetTimeValue - now;
+    //    document.getElementById("duration").innerHTML = duration;
+
+    //    setTimeout(tickCountdown, 100);
+    //}
+
+    function tickCountdown() {
+        let now = new Date();
+        let targetDateControl = document.getElementById("target-date");
+        let targetTimeControl = document.getElementById("target-time");
+
+        let targetDateValue = targetDateControl.valueAsDate;
+        let targetTimeValue = targetTimeControl.valueAsDate;
+
+        targetDateValue.setHours(targetDateValue.getHours() + targetDateValue.getTimezoneOffset()/60);
+        targetTimeValue.setHours(targetTimeValue.getHours() + targetTimeValue.getTimezoneOffset()/60);
+
+
+        document.getElementById("duration").innerHTML = typeof (targetTimeValue);
+        targetTimeValue.setFullYear(targetDateValue.getFullYear());
+        targetTimeValue.setMonth(targetDateValue.getMonth());
+        targetTimeValue.setDate(targetDateValue.getDate());
+
+        document.getElementById("target-date-value").innerHTML = targetDateValue;
+        document.getElementById("target-time-value").innerHTML = targetTimeValue;
+        document.getElementById("current-time-value").innerHTML = now;
+
+        let duration = targetTimeValue - now;
+        document.getElementById("duration").innerHTML = duration;
+
+        let timestamp = Math.trunc(duration / 1000);
+        document.getElementById("timestamp").innerHTML = timestamp;
+
+        setTimeout(tickCountdown, 100);
+    }
+
 }
